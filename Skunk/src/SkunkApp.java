b694f53947a44223800aaf80f1b059f4c76350ef
Robert Roy <robertr@visi.com>
2018-03-23 16:14:24 -0500

    Added the rules to the startup menu

--- Skunk/src/SkunkApp.java
+++ Skunk/src/SkunkApp.java
@@ -90,23 +90,70 @@ public class SkunkApp
 	{
 		return this.justOneGame;
 	}
+	
+	public void DumpSkunkRules()
+	{
+		this.ui.DisplayMsg("********************************************",true);
+		this.ui.DisplayMsg("***     The Rules of the SKUNK game!!    ***", true);
+		this.ui.DisplayMsg("********************************************",true);
+		this.ui.DisplayMsg(" ", true);
+		this.ui.DisplayMsg("DIRECTIONS FOR PLAYING:", true);
+
+		this.ui.DisplayMsg("The object of the game is to accumulate a score of ", true);
+		this.ui.DisplayMsg("100 points or more. A score is made by rolling the ", true);
+		this.ui.DisplayMsg("dice and combining the points on the two dice. ", true);
+		this.ui.DisplayMsg("For example: A 4 and 5 would be 9 points - if  ", true);
+		this.ui.DisplayMsg("the player decides to take another roll of the  ", true);
+		this.ui.DisplayMsg("dice and turns up a 3 and 5 (8 points), he would then  ", true);
+		this.ui.DisplayMsg("have an accumulated total of 17 points for the two rolls. ", true);
+		this.ui.DisplayMsg("The player has the privilege of continuing to shake to  ", true);
+		this.ui.DisplayMsg("increase his score or of passing the dice to wait for the ", true);
+		this.ui.DisplayMsg("next series, thus preventing thepossibility of rolling a", true);
+		this.ui.DisplayMsg("Skunk and losing his score.", true);
+		this.ui.DisplayMsg(" ", true);
+		this.ui.DisplayMsg("PENALTIES:", true);
+
+		this.ui.DisplayMsg("A skunk in any series voids the score for that series ", true);
+		this.ui.DisplayMsg("only and draws a penalty of 1 chip placed in the kitty," , true);
+		this.ui.DisplayMsg("and loss of dice.", true);
+		this.ui.DisplayMsg(" ", true);
+		this.ui.DisplayMsg("A skunk and a deuce voids the score for that series only" , true); 
+		this.ui.DisplayMsg("and draws a penalty of 2 chips placed in the \"kitty\", and ",true);
+		this.ui.DisplayMsg("loss of dice.", true);
+		this.ui.DisplayMsg(" ", true);
+		this.ui.DisplayMsg("TWO skunks void the ENTIRE accumulated score and draws a ", true);
+		this.ui.DisplayMsg("penalty of 4 chips placed in the \"kitty\", and loss of dice.", true);
+		this.ui.DisplayMsg("Player must again start to score from scratch.",true);
+		this.ui.DisplayMsg(" ", true);
+
+		
+		this.ui.DisplayMsg("********************************************",true);		
+	}
 	//
 	// Setup the Match
 	public int SetupSkunkMatch()
 	{
 		int rc = 0;
 		int prompt;
-		String[] menuMsgs = new String[3];
+		String[] menuMsgs = new String[4];
 		
 		menuMsgs[0] = "Play one Game of Skunk";
 		menuMsgs[1] = "Play games until winner";
-		menuMsgs[2] = "Quit";
+		menuMsgs[2] = "Review the rules for Skunk game";
+		menuMsgs[3] = "Quit";
 				
 		this.ui.DisplayMsg("********************************************",true);
-		this.ui.DisplayMsg("*** Welcome to the game of SKUNK!!  ***", true);
+		this.ui.DisplayMsg("***   Welcome to the game of SKUNK!!    ***", true);
 		this.ui.DisplayMsg("********************************************",true);
 		
-		prompt = this.ui.DisplayMenuPrompt("Skunk Match Options", menuMsgs, 3);		
+		do {
+			prompt = this.ui.DisplayMenuPrompt("Skunk Match Options", menuMsgs, 4);		
+		
+			if(prompt == 3) {
+				this.DumpSkunkRules();
+			}
+		}while(prompt == 3);
+			
 		
 		if(prompt == 2) {
 			this.justOneGame = false;
@@ -114,7 +161,8 @@ public class SkunkApp
 			this.justOneGame = true;
 		}else {
 			rc = -1;
-		}		
+		}	
+		
 		
 		return rc;
 	}
@@ -204,32 +252,36 @@ public class SkunkApp
 				
 			numPlayers = match.SetupNumPlayers();
 			
-			if(match.SetupPlayers(numPlayers) == numPlayers) {
-			
-				match.SetupGame();
-				//
-				// Play until all one winner, or a quit is
-				// Given
-				while(status == 0) {
-					match.PlayOneGame();			
-										
-					match.DumpPlayerScores();
+			if(numPlayers > 0) {
+				if(match.SetupPlayers(numPlayers) == numPlayers) {
+				
+					match.SetupGame();
 					//
-					// Determine if we should play more
-					if(match.GetJustOneGameFlag() == false) {
+					// Play until all one winner, or a quit is
+					// Given
+					while(status == 0) {
+						match.PlayOneGame();			
+											
+						match.DumpPlayerScores();
 						//
-						// Do they want to play another game?
-						if(ui.DisplayYesNoPrompt("Play Another game?") == false){
-							status = -1;
+						// Determine if we should play more
+						if(match.GetJustOneGameFlag() == false) {
+							//
+							// Do they want to play another game?
+							if(ui.DisplayYesNoPrompt("Play Another game?") == false){
+								status = -1;
+							}else {
+								
+							}
 						}else {
-							
+							status = -1; /* Just Playing one Game */
 						}
-					}else {
-						status = -1; /* Just Playing one Game */
-					}
-				}	
+					}	
+				}else {
+					ui.DisplayMsg("**** CRITICAL ERROR - Failed to Create Players ****", true);
+				}
 			}else {
-				ui.DisplayMsg("**** CRITICAL ERROR - Failed to Create Players ****", true);
+				ui.DisplayMsg("**** CRITICAL ERROR - Invalid Number of Players ****", true);
 			}
 		}		
 		
